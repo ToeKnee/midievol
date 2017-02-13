7738b3854c43275cf240fd316b7daeab1e6b7eb2
Anthony Batchelor <tony@ynottony.net>
2017-02-13 21:01:26 +0000

    Init drums to 16 beats in length

--- src/Evol/drum.ino
+++ src/Evol/drum.ino
@@ -114,7 +114,7 @@ void init_drums() {
     drum_sequence.channel = 10;
     drum_sequence.beat_division = sixteenth;
     for (int x = 0; x < 16; x++){
-        euclidean_build(x, 0, 4, 0, 35 + x);
+        euclidean_build(x, 0, 16, 0, 35 + x);
         drum_track_edit_mode[x] = BEATS;
     }
     loadDrumSequence(true);
